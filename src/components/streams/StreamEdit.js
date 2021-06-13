e84b38dd2d43d7633bd1b2c6bd2f4543aac2ded8
Borislav Slavov <bslavov@nemetschek.bg>
2021-06-13 23:16:16 +0300

    form create, edit

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
-const StreamEdit = () => {
-    return <div>StreamEdit</div>;
+const StreamEdit = (props) => {
+
+    const initialFetchStream = () => {
+        props.fetchStream(props.match.params.id);
+    };
+
+    useEffect(initialFetchStream, []);
+
+    const onSubmit = (formValues) => {
+        props.editStream(props.match.params.id, formValues);
+    }
+
+    if (!props.stream) {
+        return <div>Loading...</div>;
+    }
+
+    return (<div>
+        <h3>Edit a stream</h3>
+        <StreamForm
+            initialValues={_.pick(props.stream, 'title', 'description')}
+            onSubmit={onSubmit}
+        />
+    </div>);
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchStream, editStream })
+    (StreamEdit);
